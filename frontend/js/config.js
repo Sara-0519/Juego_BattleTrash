24b5066ac92db1365c76b06b67057e36fa8c55fc
Sara Correa <saritac.serna@gmail.com>
2025-10-31 20:32:06 -0500

    final0.4

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,8 +1,8 @@
 ﻿const CONFIG = {
     API_URL: 
         window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
-        ? 'http://localhost:3000'
-        : 'https://juego-battletrashintento.onrender.com',  
+        ? 'http://localhost:3000/api'
+        : 'https://juego-battletrashintento.onrender.com/api',  
     POINTS: {
         CORRECT: 100,
         INCORRECT: -20
